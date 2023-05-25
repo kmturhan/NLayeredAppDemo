a35b75a0a6da6ccb2cc58926b5e6677fe70c6891
kmturhan <kmturhan@gmail.com>
2023-05-25 15:42:32 +0300

    dependency injection added

--- NLayeredAppDemo/Northwind.WebFormsUI/Form1.cs
+++ NLayeredAppDemo/Northwind.WebFormsUI/Form1.cs
@@ -1,5 +1,6 @@
 using Northwind.Business.Abstract;
 using Northwind.Business.Concrete;
+using Northwind.Business.DependencyResolvers.Ninject;
 using Northwind.DataAccess.Concrete.EntityFramework;
 using Northwind.DataAccess.Concrete.NHibernate;
 using Northwind.Entities.Concrete;
@@ -13,8 +14,9 @@ namespace Northwind.WebFormsUI
 		public Form1()
 		{
 			InitializeComponent();
-			_productService = new ProductManager(new EfProductDal());
-			_categoryService = new CategoryManager(new EfCategoryDal());
+			_productService = InstanceFactory.GetInstance<IProductService>();
+			//_categoryService = new CategoryManager(new EfCategoryDal());
+			_categoryService = InstanceFactory.GetInstance<ICategoryService>();
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
